515e980b4bd4c1202adf534bfeef5ddf2fcef3cc
notfolder <notfolder@gmail.com>
2025-04-17 22:47:05 +0900

    不要タブの整理

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,65 +1,29 @@
 <template>
   <v-container>
     <v-tabs v-model="tab">
-      <v-tab value="test1">test1</v-tab>
-      <v-tab value="test2">test2</v-tab>
-      <v-tab value="test3">test3</v-tab>
-      <v-tab value="test4">test4</v-tab> <!-- test4タブを追加 -->
-      <v-tab value="test5">test5</v-tab> <!-- test5タブを追加 -->
+      <v-tab value="suggestedInput">補完入力</v-tab>
+      <v-tab value="suggestionEdit">単語入力</v-tab>
     </v-tabs>
     <v-tabs-window v-model="tab">
-      <v-tabs-window-item value="test1">
-        <div>test1</div>
-        <v-radio-group v-model="generation">
-          <v-radio label="世代1" value="gen1"></v-radio>
-          <v-radio label="世代2" value="gen2"></v-radio>
-        </v-radio-group>
-        {{ generation }}
+      <v-tabs-window-item value="suggestedInput">
+        <TextInputPage :suggestions="suggestions" v-model="textInputContent" />
       </v-tabs-window-item>
-      <v-tabs-window-item value="test2">
-        <div>test2</div>
-        <v-select multiple :items="products" label="製品" v-model="product">
-          {{ product }}
-        </v-select>
-        <div>{{ product }}</div>
-      </v-tabs-window-item>
-      <v-tabs-window-item value="test3">
-        <div>test3</div>
-        <v-container>
-          <v-row>
-            <v-col>
-              <v-data-table v-model:items-per-page="itemsPerPage"
-              :headers="headers"
-              :items="wafers"
-              :items-per-page-options="pages"
-              items-per-page-text="表示行数">
-                <template v-slot:item.id="{ item }">
-                  <a :href="item.id"> {{ item.id }}</a> 
-                </template>
-              </v-data-table>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-tabs-window-item>
-      <v-tabs-window-item value="test4"> <!-- test4タブの内容 -->
-        <TextInputPage :suggestions="suggestions" v-model="textInputContent" /> <!-- TextInputPageにv-modelを追加 -->
-      </v-tabs-window-item>
-      <v-tabs-window-item value="test5"> <!-- test5タブの内容 -->
-        <SuggestionsEditPage v-model="suggestions" /> <!-- SuggestionsEditPageコンポーネントにv-modelでsuggestionsをバインド -->
+      <v-tabs-window-item value="suggestionEdit">
+        <SuggestionsEditPage v-model="suggestions" />
       </v-tabs-window-item>
     </v-tabs-window>
-    <v-textarea label="suggestテキスト内容" v-model="textInputContent"></v-textarea> <!-- 最後のテキストエリアにv-modelを追加 -->
+    <v-textarea label="suggestテキスト内容" v-model="textInputContent"></v-textarea>
   </v-container>
 </template>
 
 <script>
-import TextInputPage from './TextInputPage.vue'; // TextInputPageをインポート
-import SuggestionsEditPage from './SuggestionsEditPage.vue'; // SuggestionsEditPageをインポート
+import TextInputPage from './TextInputPage.vue';
+import SuggestionsEditPage from './SuggestionsEditPage.vue';
 
 export default {
   components: {
-    TextInputPage, // コンポーネントを登録
-    SuggestionsEditPage, // コンポーネントを登録
+    TextInputPage,
+    SuggestionsEditPage,
   },
   data: () => ({
     tab: null,
